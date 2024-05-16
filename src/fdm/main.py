98a4f946dc130226dfe50d148e50ab6f4af7ab47
Yooshin Oh <stevenoh0908@gmail.com>
2024-05-16 18:28:43 +0900

    First Work on fdm/util.py

---
+++ src/fdm/main.py
@@ -0,0 +1,13 @@
+'''
+-----
+fdm/main.py
+-----
+Created: 2024-05-16 06:14:14
+Author: Yooshin Oh (stevenoh0908@snu.ac.kr)
+-----
+Last Modified: 2024-05-16 06:14:26
+Modified By: Yooshin Oh (stevenoh0908@snu.ac.kr)
+-----
+- Finite Difference Method Main Module
+'''
+
