Corrected J Calculation with Its Mass * 다만 가장 위쪽 대기에서 온도가 터지므로 원인 분석 후 대처법 찾을 것.

src/common/datastructures.py
@@ -5,7 +5,7 @@ common/datastructures.py
 Created: 2024-05-14 11:16:31
 Author: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
-Last Modified: 2024-05-16 11:34:01
+Last Modified: 2024-05-17 03:16:57
 Modified By: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
 - Defines and Manages Common Data Structures Among the Whole Model.
@@ -24,6 +24,7 @@ class ModelIOConfig:
     aSwProfilePath: str = "./data/a_sw_profile.txt"
     aLwProfilePath: str = "./data/a_lw_profile.txT"
     initTProfilePath: str = "./data/init_T_profile.txt"
+    densityProfilePath: str = "./data/density_profile.txt"
     outputPath: str = "./output"
     pass
 
@@ -51,4 +52,5 @@ class ModelData:
     RProfile: np.ndarray = np.zeros(1)
     aSwProfile: np.ndarray = np.zeros(1)
     aLwProfile: np.ndarray = np.zeros(1)
+    densityProfile: np.ndarray = np.zeros(1)
     pass

src/load/main.py
@@ -5,8 +5,8 @@ load/main.py
 Created: 2024-05-14 11:49:56
 Author: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
-Last Modified: 2024-05-17 01:17:32
-Last Modified: 2024-05-17 01:17:32
+Last Modified: 2024-05-17 03:38:54
+Last Modified: 2024-05-17 03:38:54
 Modified By: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
 - Main Load Module.
@@ -112,6 +112,7 @@ class Driver:
         modelIOConfig.aSwProfilePath = config_io['aSwProfilePath']
         modelIOConfig.aLwProfilePath = config_io['aLwProfilePath']
         modelIOConfig.initTProfilePath = config_io['initTProfilePath']
+        modelIOConfig.densityProfilePath = config_io['densityProfilePath']
         modelIOConfig.outputPath = config_io['outputPath']
         config_structure = config['modelStructureConfig']
         modelStructureConfig.dz = float(config_structure['dz'])
@@ -147,6 +148,7 @@ class Driver:
         self.modelData.RProfile = np.zeros(self.modelConfig.modelStructureConfig.nz-1, dtype=np.float32)
         self.modelData.aSwProfile = np.zeros(self.modelConfig.modelStructureConfig.nz-1, dtype=np.float32)
         self.modelData.aLwProfile = np.zeros(self.modelConfig.modelStructureConfig.nz-1, dtype=np.float32)
+        self.modelData.densityProfile = np.zeros(self.modelConfig.modelStructureConfig.nz-1, dtype=np.float32)
         loader = Loader()
         # Load InitTemp
         loader.open(self.modelConfig.modelIOConfig.initTProfilePath)
@@ -156,11 +158,9 @@ class Driver:
         loader.open(self.modelConfig.modelIOConfig.cPProfilePath)
         loader.loadInto(self.modelData.cPProfile[:])
         loader.close()
-        self.modelData.cPProfile[0] = FILL_VALUE
         # Load R Profile
         loader.open(self.modelConfig.modelIOConfig.RProfilePath)
         loader.loadInto(self.modelData.RProfile[:])
-        self.modelData.RProfile[0] = FILL_VALUE
         loader.close()
         # Load aSw Profile
         loader.open(self.modelConfig.modelIOConfig.aSwProfilePath)
@@ -169,6 +169,10 @@ class Driver:
         # Load aLw Profile
         loader.open(self.modelConfig.modelIOConfig.aLwProfilePath)
         loader.loadInto(self.modelData.aLwProfile[:])
+        loader.close()
+        # Load Density Profile
+        loader.open(self.modelConfig.modelIOConfig.densityProfilePath)
+        loader.loadInto(self.modelData.densityProfile[:])
         loader.close()
         # commit changes and return
         return self.modelData

src/rt/longwave_forcing.py
@@ -5,7 +5,7 @@ rt/longwave_forcing.py
 Created: 2024-05-17 12:07:43
 Author: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
-Last Modified: 2024-05-17 01:13:12
+Last Modified: 2024-05-17 03:22:47
 Modified By: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
 - LW Forcing Calculation Methods
@@ -34,6 +34,7 @@ def atmo_forcing(modelData, modelConfig, timestep=1):
     dy = modelConfig.modelStructureConfig.dy
     forcing = np.zeros(nz, dtype=np.float32)
     for iz in range(1, nz):
+        mass = modelConfig.modelStructureConfig.dx * modelConfig.modelStructureConfig.dy * modelConfig.modelStructureConfig.dz * modelData.densityProfile[iz-1]
         coef = modelData.aLwProfile[iz-1] * STEFAN_BOLTZMANN_CONST * dx * dy
         first_arg = 0.
         for k in range(1, iz):
@@ -45,7 +46,7 @@ def atmo_forcing(modelData, modelConfig, timestep=1):
             pass
         third_arg = 2*modelData.temperature[timestep,iz]**4
         surf_arg = STEFAN_BOLTZMANN_CONST * dx * dy * (modelData.temperature[timestep, 0]**4) * util.transmisttance(modelData, modelConfig, startIdx=0, endIdx=iz-1)
-        forcing[iz] = coef*(first_arg+second_arg-third_arg) + surf_arg
+        forcing[iz] = (coef*(first_arg+second_arg-third_arg) + surf_arg) / mass
         pass
     return forcing[1:]
 

src/rt/shortwave_forcing.py
@@ -5,7 +5,7 @@ rt/shortwave_forcing.py
 Created: 2024-05-17 12:07:37
 Author: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
-Last Modified: 2024-05-17 12:57:33
+Last Modified: 2024-05-17 03:22:16
 Modified By: Yooshin Oh (stevenoh0908@snu.ac.kr)
 -----
 - SW Forcing Calculation Methods
@@ -29,9 +29,10 @@ def atmo_forcing(modelData, modelConfig, timestep=1):
     coef = SOLAR_CONSTANT * modelConfig.modelStructureConfig.dx * modelConfig.modelStructureConfig.dy
     forcing = np.zeros(nz, dtype=np.float32)
     for iz in range(1, nz):
+        mass = modelConfig.modelStructureConfig.dx * modelConfig.modelStructureConfig.dy * modelConfig.modelStructureConfig.dz * modelData.densityProfile[iz-1]
         first_arg = util.transmisttance(modelData, modelConfig, startIdx=iz, endIdx=nz, type=util.TYPE_SW)-util.transmisttance(modelData, modelConfig, startIdx=iz-1, endIdx=nz, type=util.TYPE_SW)
         second_arg = util.transmisttance(modelData, modelConfig, startIdx=0, endIdx=nz, type=util.TYPE_SW)*(util.transmisttance(modelData, modelConfig, startIdx=0, endIdx=iz-1, type=util.TYPE_SW)-util.transmisttance(modelData, modelConfig, startIdx=0, endIdx=iz, type=util.TYPE_SW))
-        forcing[iz] = coef*(first_arg+second_arg)
+        forcing[iz] = (coef*(first_arg+second_arg))/mass
         pass
     return forcing[1:]
 